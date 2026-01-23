7803bf8fcff8e71b3a3bdf0867de8501bb56610b
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2026-01-22 21:50:05 -0600

    feat: created initial script for bed project directory (template version)

---
+++ scripts/choncc.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "You are actually a lazy chud. Whats the magic word?"
+read word
+capitalized_input="${word^}"
+
+while [ $capitalized_input != "Please" ]
+do 
+    clear
+	echo "Guess you actually have to work. I don't think we want to stay here forever waiting for a simple word..."
+    read word
+    capitalized_input="${word^}"
+done
+echo
+echo "Fine, I'll configure your project directory for you..."
+echo
+echo "Stealing template from Dave. Poor Dave."
+echo "Stole template."
+echo
+
+read -p "Name the repo please: " repo_name
+echo "Sigh... creating repo with the name: ${repo_name}, I guess. "
+echo
+gh repo create $repo_name -p DaveRRC/BED-template -c --private
+echo
+echo "Repo created (i think?)"
+echo "Also cloned repository locally (this is why i'm the goat)."
