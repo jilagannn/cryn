149045f0fcc9eb164994cf69869ab70255069219
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2026-02-10 00:22:21 -0600

    init: initialized burno foundation

--- scripts/burno.py
+++ scripts/burno.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """This module defines the burno script.
 
 This module utilizes the gmail API to delete or trash emails in
@@ -9,6 +10,9 @@ __version__ = "2.13.2026"
 
 import os
 import time
+=======
+import os.path
+>>>>>>> c2a89a0 (init: initialized burno foundation)
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -16,6 +20,7 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+<<<<<<< HEAD
 # Scope for full access
 SCOPES = ["https://mail.google.com/"]
 
@@ -343,3 +348,52 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+=======
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+
+def main():
+  """Shows basic usage of the Gmail API.
+  Lists the user's Gmail labels.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("configs/gmail/token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "configs/gmail/credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    # Call the Gmail API
+    service = build("gmail", "v1", credentials=creds)
+    results = service.users().labels().list(userId="me").execute()
+    labels = results.get("labels", [])
+
+    if not labels:
+      print("No labels found.")
+      return
+    print("Labels:")
+    for label in labels:
+      print(label["name"])
+
+  except HttpError as error:
+    # TODO(developer) - Handle errors from gmail API.
+    print(f"An error occurred: {error}")
+
+
+if __name__ == "__main__":
+  main()
+>>>>>>> c2a89a0 (init: initialized burno foundation)
