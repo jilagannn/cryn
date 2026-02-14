39399fb255520c497b00c6d8a1b172d90de3d9d0
Jheyrus Ilagan <jllagan@academic.rrc.ca>
2026-02-14 00:53:01 -0600

    feat: created method for category select deletion

--- scripts/burno.py
+++ scripts/burno.py
@@ -77,8 +77,8 @@ def get_labels() -> None:
 
 def count_trash() -> int:
     try:
-        trash = service.users().messages().list(userId="me", 
-                                                q="in:trash").execute()
+        trash = (service.users().messages()
+                 .list(userId="me", q="in:trash").execute())
         trash_count = trash["resultSizeEstimate"]
         trash_message = f"Total emails in trash (estimate): {trash_count}"
         print(trash_message)
@@ -87,8 +87,8 @@ def count_trash() -> int:
 
 def clear_trash():
     try:
-        trash = service.users().messages().list(userId="me", 
-                                                q="in:trash").execute()
+        trash = (service.users().messages()
+                 .list(userId="me", q="in:trash").execute())
         messages = trash.get("messages", [])
         trash_count = trash["resultSizeEstimate"]
         if messages:
@@ -98,8 +98,8 @@ def clear_trash():
             if user_confirmation.capitalize() == "Y":
                 print("Deleting emails in Trash.")
                 for message in messages:
-                    service.users().messages().delete(userId="me", 
-                                                      id=message["id"]).execute()
+                    (service.users().messages()
+                     .delete(userId="me", id=message["id"]).execute())
             print("Emails in Trash deleted!")
         else:
             print("Trash is empty or has been cleared! already")
@@ -108,8 +108,8 @@ def clear_trash():
 
 def count_spam() -> int:
     try:
-        spam = service.users().messages().list(userId="me", 
-                                                q="in:spam").execute()
+        spam = (service.users().messages()
+                .list(userId="me", q="in:spam").execute())
         spam_count = spam["resultSizeEstimate"]
         spam_message = f"Total emails in spam (estimate): {spam_count}"
         print(spam_message)
@@ -118,8 +118,8 @@ def count_spam() -> int:
 
 def clear_spam():
     try:
-        spam = service.users().messages().list(userId="me", 
-                                                q="in:spam").execute()
+        spam = (service.users().messages()
+                .list(userId="me", q="in:spam").execute())
         messages = spam.get("messages", [])
         spam_count = spam["resultSizeEstimate"]
         if messages:
@@ -129,15 +129,97 @@ def clear_spam():
             if user_confirmation.capitalize() == "Y":
                 print("Deleting emails in Spam.")
                 for message in messages:
-                    service.users().messages().delete(userId="me", 
-                                                      id=message["id"]).execute()
+                    (service.users().messages()
+                     .delete(userId="me", id=message["id"]).execute())
                 print("Emails in Spam deleted!")
         else:
             print("Spam is empty or has been cleared! already")
     except HttpError as error:
         print(f"An error occurred: {error}")
 
+def select_category():
+    CATEGORY_OPTIONS = (f"1. Promos\n"
+                    f"2. Social\n"
+                    f"3. Update\n"
+                    f"4. Exit\n")
+    print(CATEGORY_OPTIONS)
+    try:
+        user_choice = int(input("Please select a category: "))
+
+        if user_choice == 1:
+            promos = (service.users().messages()
+                    .list(userId="me", q="category:promotions").execute())
+            promos_messages = promos.get("messages", [])
+            print(len(promos_messages))
+            promos_estimate_count = promos["resultSizeEstimate"]
+            if promos_messages:
+                print(f"There are {promos_estimate_count} in promos to be deleted.")
+                user_confirmation = input(f"Are you sure you want to delete "
+                                        f"selected emails? y/n: ")
+                if user_confirmation.capitalize() == "Y":
+                    print("Deleting emails in Promos.")
+                    for message in promos_messages:
+                        (service.users().messages()
+                        .delete(userId="me", id=message["id"]).execute())
+                    print("Emails in Promos deleted!")
+            else:
+                print("Promos is empty or has been cleared! already")
+
+        if user_choice == 2:
+            socials = (service.users().messages()
+                    .list(userId="me", q="category:social").execute())
+            socials_messages = socials.get("messages", [])
+            socials_estimate_count = socials["resultSizeEstimate"]
+            if socials_messages:
+                print(f"There are {socials_estimate_count} in socials to be "
+                      f"deleted.")
+                user_confirmation = input(f"Are you sure you want to delete "
+                                        f"selected emails? y/n: ")
+                if user_confirmation.capitalize() == "Y":
+                    print("Deleting emails in Socials.")
+                    for message in socials_messages:
+                        (service.users().messages()
+                        .delete(userId="me", id=message["id"]).execute())
+                    print("Emails in Socials deleted!")
+            else:
+                print("Socials is empty or has been cleared! already")
+
+        if user_choice == 3:
+            updates = (service.users().messages()
+                    .list(userId="me", q="category:updates AND is:unread").execute())
+            updates_messages = updates.get("messages", [])
+            print(len(updates_messages))
+            updates_estimate_count = updates["resultSizeEstimate"]
+            if updates_messages:
+                print(f"There are {updates_estimate_count} in updates to be "
+                      f"deleted.")
+                user_confirmation = input(f"Are you sure you want to delete "
+                                        f"selected emails? y/n: ")
+                if user_confirmation.capitalize() == "Y":
+                    print("Deleting emails in Socials.")
+                    for message in updates_messages:
+                        (service.users().messages()
+                        .delete(userId="me", id=message["id"]).execute())
+                    print("Emails in Updates deleted!")
+            else:
+                print("Updates is empty or has been cleared! already")
+
+        if user_choice == 4:
+            print("Returning to main selection!")
+            return
+        
+        else:
+            print("Please select a valid category")
+
+    except (HttpError, TypeError) as error:
+        print(f"An error occurred: {error}")
+
+
+
+
 def main():
+    get_labels()
+    select_category()
     count_spam()
     clear_spam()
     count_trash()
